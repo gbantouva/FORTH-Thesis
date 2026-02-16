9383003dda1b0dd7bcb167aa1d05d46317a60fbf
gbantouva <64810316+gbantouva@users.noreply.github.com>
2026-02-16 22:40:58 +0200

    Create signals_test.m

---
+++ src/connectivity/test/signals_test.m
@@ -0,0 +1,38 @@
+function x=signals_test(db_noise)
+
+%load erp_signal.mat; x_erp=x';  
+x=zeros(2000,15);
+
+A=10;
+phase_const=pi/4;
+samples_delay=2;
+N=1000;
+start_point=100;
+
+
+
+period_1=1:1100; period_2=1101:1100; period_3=1101:1200;
+period_1_plus=1:(length(period_1)+samples_delay);
+
+f1=10; f2=20; f3=15;
+
+
+xs1(:,1)=A*cos(2*pi*(1:1200)*f1/1000); 
+
+%A12=1; A13=0; A23=1; A24=0; A34=0; A14=1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x(period_1,1)=xs1(100+period_1,1); 
+xs=x(:,1);
+%xs = 10*chirp((0.001:0.001:1.1),8,1,30)';
+%xs(1:1000,1)=x_erp(:,1); xs(1001:1100,1)=0;
+
+x(1:1100,1)=xs(1:1100,1);   x(:,1)=awgn(x(:,1),db_noise,'measured','db');
+
+x(samples_delay+1:1100,2)=x(1:1100-samples_delay,1); x(:,2)=awgn(x(:,2),db_noise,'measured','db');
+x(samples_delay+1:1100,3)=x(1:1100-samples_delay,2); x(:,3)=awgn(x(:,3),db_noise,'measured','db');
+x(samples_delay+1:1100,4)=x(1:1100-samples_delay,3); x(:,4)=awgn(x(:,4),db_noise,'measured','db');
+
+x(:,5)=0+0.5*wgn(2000,1,0);
+
+x=x(start_point:end,:);
